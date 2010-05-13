d0a433971cce03f9e88ea9f5242cbaf624870049
Dirk Eddelbuettel <edd@debian.org>
2010-05-13 12:15:48 +0000

    still borked

--- src/fastLm.cpp
+++ src/fastLm.cpp
@@ -26,32 +26,38 @@
 
 extern "C" SEXP fastLm(SEXP ys, SEXP Xs) {
 
-    Rcpp::NumericVector Yr(ys);			// creates Rcpp vector from SEXP
-    Rcpp::NumericMatrix Xr(Xs);			// creates Rcpp matrix from SEXP
+    RcppGSL::vector<double> y(ys);		// creates RcppGSL vector from SEXP
+    RcppGSL::matrix<double> X(Xs);		// creates RcppGSL matrix from SEXP
 
-    int i,j,n = Xr.nrow(), k = Xr.ncol();
+    //Rcpp::NumericVector Yr(ys);			
+    //Rcpp::NumericMatrix Xr(Xs);			// creates Rcpp matrix from SEXP
+
+    //int i, j, n = Xr.nrow(), k = Xr.ncol();
+    int i, j, n=4,k=2;  // FIXME -- need coef extraction
     double chisq;
 
-    gsl_matrix *X = gsl_matrix_alloc (n, k);
-    gsl_vector *y = gsl_vector_alloc (n);
-    gsl_vector *c = gsl_vector_alloc (k);
+    //gsl_matrix *X = gsl_matrix_alloc (n, k);
+    //gsl_vector *y = gsl_vector_alloc (n);
+    gsl_vector *c = gsl_vector_alloc (k);   // FIXME trivial ctor
     gsl_matrix *cov = gsl_matrix_alloc (k, k);
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < k; j++)
-            gsl_matrix_set (X, i, j, Xr(i,j));
-        gsl_vector_set (y, i, Yr(i));
-    }
+    //for (i = 0; i < n; i++) {
+    //    for (j = 0; j < k; j++)
+    //        gsl_matrix_set (X, i, j, Xr(i,j));
+    //    gsl_vector_set (y, i, Yr(i));
+    //}
 
     gsl_multifit_linear_workspace *work = gsl_multifit_linear_alloc (n, k);
     gsl_multifit_linear (X, y, c, cov, &chisq, work);
     gsl_multifit_linear_free (work);
 
+	// FIXME need free()
 
-    //Rcpp::NumericVector coefr = Rcpp::wrap(c);
-    RcppGSL::vector<double> coefr(5);
-    RcppGSL::vector<double> vec(ys);
-    //return Rcpp::List::create( Rcpp::Named( "coef", coefr),
-    //                           Rcpp::Named( "stderr", coefr));
+	//Rcpp::NumericVector coefr = Rcpp::wrap(c);
+    //RcppGSL::vector<double> coefr(5);
+    return Rcpp::List::create( Rcpp::Named( "coef", c),
+                               Rcpp::Named( "stderr", c));
+    //return Rcpp::List::create( Rcpp::_["coef"] = c,
+    //                           Rcpp::_["stderr"], c);
     return Xs;
 }
 
