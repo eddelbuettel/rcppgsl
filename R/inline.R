15622bcf053ea25bd291632f6bae7e7ac6f5c7b7
Dirk Eddelbuettel <edd@debian.org>
2013-10-20 23:10:59 +0000

    fix a copy-and-pasto error (and trigger an R-Forge rebuild as well)

--- R/inline.R
+++ R/inline.R
@@ -13,7 +13,7 @@
 ## GNU General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public License
-## along with RcppArmadillo.  If not, see <http://www.gnu.org/licenses/>.
+## along with RcppGSL.  If not, see <http://www.gnu.org/licenses/>.
 
 .pkgglobalenv <- new.env(parent=emptyenv())
 
