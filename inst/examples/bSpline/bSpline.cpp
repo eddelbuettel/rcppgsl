8132c4494ad47d5b62e7953c103c6dcc12b5faf4
Dirk Eddelbuettel <edd@debian.org>
2015-08-20 08:11:24 -0500

    updates bSpline example to show RcppGSL features

--- inst/examples/bSpline/bSpline.cpp
+++ inst/examples/bSpline/bSpline.cpp
@@ -11,46 +11,37 @@
 
 const int N = 200;                              // number of data points to fit 
 const int NCOEFFS = 12;                         // number of fit coefficients */
-const int NBREAK = (NCOEFFS - 2);               // nbreak = ncoeffs + 2 - k = ncoeffs - 2 since k = 4 */
+const int NBREAK = (NCOEFFS - 2);               // ncoeffs + 2 - k = ncoeffs - 2 as k = 4 */
 
 // [[Rcpp::export]]
 Rcpp::List genData() {
-
     const size_t n = N;
     size_t i;
-    double dy;
-    gsl_rng *r;
-    RcppGSL::vector<double> w(n), x(n), y(n);
+    RcppGSL::Vector w(n), x(n), y(n);
 
     gsl_rng_env_setup();
-    r = gsl_rng_alloc(gsl_rng_default);
-
+    gsl_rng *r = gsl_rng_alloc(gsl_rng_default);
     //printf("#m=0,S=0\n");
-    /* this is the data to be fitted */
 
-    for (i = 0; i < n; ++i) {
-        double sigma;
+    for (i = 0; i < n; ++i) {      /* this is the data to be fitted */
         double xi = (15.0 / (N - 1)) * i;
         double yi = cos(xi) * exp(-0.1 * xi);
 
-        sigma = 0.1 * yi;
-        dy = gsl_ran_gaussian(r, sigma);
+        double sigma = 0.1 * yi;
+        double dy = gsl_ran_gaussian(r, sigma);
         yi += dy;
 
-        gsl_vector_set(x, i, xi);
-        gsl_vector_set(y, i, yi);
-        gsl_vector_set(w, i, 1.0 / (sigma * sigma));
-                
+        x[i] = xi;  // instead of  gsl_vector_set(x, i, xi);
+        y[i] = yi;  // instead of  gsl_vector_set(y, i, yi);
+        w[i] = 1.0 / (sigma * sigma);    // gsl_vector_set(w, i, 1.0 / (sigma * sigma));
         //printf("%f %f\n", xi, yi);
     }
 
-    Rcpp::DataFrame res = Rcpp::DataFrame::create(Rcpp::Named("x") = x,
-                                                  Rcpp::Named("y") = y,
-                                                  Rcpp::Named("w") = w);
-
     gsl_rng_free(r);
-
-    return(res);
+    
+    return Rcpp::DataFrame::create(Rcpp::Named("x") = x,
+                                   Rcpp::Named("y") = y,
+                                   Rcpp::Named("w") = w);
 }
 
 
@@ -64,35 +55,31 @@ Rcpp::List fitData(Rcpp::DataFrame D) {
     const size_t n = N;
     size_t i, j;
 
-    RcppGSL::vector<double> y = D["y"];	// access columns by name, 
-    RcppGSL::vector<double> x = D["x"];	// assigning to GSL vectors
-    RcppGSL::vector<double> w = D["w"];
+    RcppGSL::Vector y = D["y"];		// access columns by name, 
+    RcppGSL::Vector x = D["x"];		// assigning to GSL vectors
+    RcppGSL::Vector w = D["w"];
 
     gsl_bspline_workspace *bw;
-    gsl_vector *B;              	// these could be RcppGSL::... as well 
-    gsl_vector *c; 
-    gsl_matrix *X, *cov;
+    RcppGSL::Vector B(ncoeffs);
+    RcppGSL::Vector c(ncoeffs);
+    RcppGSL::Matrix X(n, ncoeffs);
+    RcppGSL::Matrix cov(ncoeffs, ncoeffs);
     gsl_multifit_linear_workspace *mw;
     double chisq, Rsq, dof, tss;
 
     bw = gsl_bspline_alloc(4, nbreak);	    // allocate a cubic bspline workspace (k = 4)
-    B = gsl_vector_alloc(ncoeffs);
-
-    X = gsl_matrix_alloc(n, ncoeffs);
-    c = gsl_vector_alloc(ncoeffs);
-    cov = gsl_matrix_alloc(ncoeffs, ncoeffs);
     mw = gsl_multifit_linear_alloc(n, ncoeffs);
 
     gsl_bspline_knots_uniform(0.0, 15.0, bw);	// use uniform breakpoints on [0, 15] 
 
     for (i = 0; i < n; ++i) {			// construct the fit matrix X 
-        double xi = gsl_vector_get(x, i);
+        double xi = x[i];       // gsl_vector_get(x, i);
 
         gsl_bspline_eval(xi, B, bw);		// compute B_j(xi) for all j 
 
         for (j = 0; j < ncoeffs; ++j) {		// fill in row i of X 
-            double Bj = gsl_vector_get(B, j);
-            gsl_matrix_set(X, i, j, Bj);
+            double Bj = B[j];  // gsl_vector_get(B, j);
+            X(i,j) = Bj;       // gsl_matrix_set(X, i, j, Bj);
         }
     }
 
@@ -111,18 +98,13 @@ Rcpp::List fitData(Rcpp::DataFrame D) {
         FY[i] = yi;
     }
 
-    Rcpp::List res = Rcpp::List::create(Rcpp::Named("X")=FX,
-                                        Rcpp::Named("Y")=FY,
-                                        Rcpp::Named("chisqdof") = Rcpp::wrap(chisq/dof),
-                                        Rcpp::Named("rsq") = Rcpp::wrap(Rsq));
-
     gsl_bspline_free(bw);
-    gsl_vector_free(B);
-    gsl_matrix_free(X);
-    gsl_vector_free(c);
-    gsl_matrix_free(cov);
     gsl_multifit_linear_free(mw);
-    
-    return(res);   
+   
+    return Rcpp::List::create(Rcpp::Named("X") = FX,
+                              Rcpp::Named("Y") = FY,
+                              Rcpp::Named("chisqdof") = Rcpp::wrap(chisq/dof),
+                              Rcpp::Named("rsq") = Rcpp::wrap(Rsq));
+
 }
 
