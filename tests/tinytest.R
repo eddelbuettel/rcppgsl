e6ed595aaccd1495beb115454fa56f509121815f
Dirk Eddelbuettel <edd@debian.org>
2019-10-19 05:50:10 -0500

    conversion to tinytest step 2: convert test runner

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,36 +1,15 @@
-## doRUnit.R --- Run RUnit tests
-##
-## with credits to package fUtilities in RMetrics
-## which credits Gregor Gojanc's example in CRAN package  'gdata'
-## as per the (now deceased) R Wiki http://wiki.r-project.org/rwiki/doku.php?id=developers:runit
-## and changed further by Martin Maechler
-## and usage across several Rcpp* package
-## and more changes by Murray Stokely in HistogramTools
-##
-## Dirk Eddelbuettel, 2010 - 2017
 
-if (requireNamespace("RUnit", quietly=TRUE) &&
-    requireNamespace("RcppGSL", quietly=TRUE)) {
-
-    library(RUnit)
-    library(RcppGSL)
+if (requireNamespace("tinytest", quietly=TRUE) &&
+    utils::packageVersion("tinytest") >= "1.0.0") {
 
     ## Set a seed to make the test deterministic
     set.seed(42)
 
-    ## Define tests
-    testSuite <- defineTestSuite(name="RcppGSL Unit Tests",
-                                 dirs=system.file("unitTests", package = "RcppGSL"),
-                                 testFuncRegexp = "^[Tt]est.+")
+    ## R makes us to this
+    Sys.setenv("R_TESTS"="")
 
-    Sys.setenv("R_TESTS"="")	    	# without this, we get (or used to get) unit test failures
-
-    tests <- runTestSuite(testSuite)    # run tests
-    printTextProtocol(tests)		# print results
-
-    ## Return success or failure to R CMD CHECK
-    if (getErrors(tests)$nFail > 0) stop("TEST FAILED!")
-    if (getErrors(tests)$nErr > 0) stop("TEST HAD ERRORS!")
-    if (getErrors(tests)$nTestFunc < 1) stop("NO TEST FUNCTIONS RUN!")
-
+    ## there are several more granular ways to test files in a tinytest directory,
+    ## see its package vignette; tests can also run once the package is installed
+    ## using the same command `test_package(pkgName)`, or by director or file
+    tinytest::test_package("RcppGSL", ncpu=getOption("Ncpus", 1))
 }
